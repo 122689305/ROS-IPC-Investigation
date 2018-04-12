82db89e86a8c6c2af327a9b2bc72c7239d08a344
YuchaoTao <yt113@duke.edu>
2018-04-11 22:57:08 -0400

    [+] init

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/yt113/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/yt113/catkin_ws/devel;/opt/ros/lunar".split(';') if "/home/yt113/catkin_ws/devel;/opt/ros/lunar" != "" else []
